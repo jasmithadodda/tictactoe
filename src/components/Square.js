c5451c0569509fb1bef5b41d492a2f1cc18ed813
jasmithadodda <jasmithadodda@gmail.com>
2024-08-16 11:23:43 +0530

    passing properties to square and handlebutton click--set X as state

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Square() {
+    const [value, setValue] = useState(null);
+
+
+  function handleClick() {
+    console.log("clicked!");
+    setValue('X')
+  }
   return (
     <>
-    <div className="square">
-      <button className="square">1</button>
-     
-    </div>
-  
-  </>  )
+      <div className="square">
+        <button onClick={handleClick} className="square">
+          {value}
+        </button>
+      </div>
+    </>
+  );
 }
